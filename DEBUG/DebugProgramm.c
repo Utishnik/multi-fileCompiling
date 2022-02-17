27036da5a1075c6ea255b84184b80ab9b41dd029
Yrik <86683130+Utishnik@users.noreply.github.com>
2022-02-17 20:56:58 +0300

    realesation Selectiv_Scanf

--- DEBUG/DebugProgramm.c
+++ DEBUG/DebugProgramm.c
@@ -1,54 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+
+void SelectivScanf(char scanfI[],)
+{
+
+}
 
 int main(void)
 {
+    char fEnterIf=0;
+    char fCICLSIGNL=0;
     char bazapath[100]={"baza.txt"};
     char code[10000]={" "};
     char path[100]={" "};
     char c[2000]={" "};
+    int indexbaza=0;
     printf("please enter file code:\n");
-    scanf("%s",path);
+    char sym=' ';
     
+    while((1)){//условие цикла не сработал
+        printf("0000000000000000000000000\n");
+        fCICLSIGNL=1;
+        fEnterIf=1;
+        scanf("%s",path);
+        if(scanf("%c",&sym)&&sym=='\n');
+    }
+    fCICLSIGNL=0;
+    fEnterIf=0;
+    //сделать так чтобы можно было вести путь к фаулу или выбрать его из предущих строк в буфере
+    FILE *f1;
+    f1=fopen(bazapath,"a");
+        fprintf(f1,"%s\n",path);
+    fclose(f1);
+    char fl=0;
+    
+
+
     //*******FILE OPEN***********
             FILE *f;
-
             f=fopen(path,"r");
-                while(!feof(f))
+                if(f!=NULL)
                 {
-                    if(fgets(c,2000,f)!=0)
+                    while(!feof(f))
                     {
-                        strcat(code,c);
+                        if(fgets(c,2000,f)!=0)
+                        {
+                            strcat(code,c);
+                        }
                     }
                 }
+                else {printf("ERROR OPEN FILE!\n");fl=1;}
             fclose(f);
-
-            if(!fopen(path,"r"))
-            {
-                printf("error open file!!!!\n");
-                return 0;
-            }
+            
     //*******FILE OPEN END*******
 
-    printf("please enter n number symbol:\n");
-    int number=0;
-    scanf("%d",&number);
-
-
-    printf("please input 1 if you want to output n character otherwise 0 if you want to output a string from this character:\n");
-
-    int vibor=0;
-
-    scanf("%d",&vibor);
-
-    if(vibor==1)
+    if(!fl)
     {
-        printf("%c",code[number]);
-    }
-    else
-    {
-        for(int i = number;i<strlen(code);i++)
-            printf("%c",code[i]);
+        printf("please enter n number symbol:\n");
+        int number=0;
+        scanf("%d",&number);
+    
+
+        printf("please input 1 if you want to output n character otherwise 0 if you want to output a string from this character:\n");
+
+        int vibor=0;
+
+        scanf("%d",&vibor);
+
+        if(vibor==1)
+        {
+            printf("%c",code[number]);
+        }
+        else
+        {
+            for(int i = number;i<strlen(code);i++)
+                printf("%c",code[i]);
+        }
     }
 }
